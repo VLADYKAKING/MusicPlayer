0a91c96b1eb80affb8cec1f62a7c28e8c1d48bea
Vlad Kislov <vadalize.vi@gmail.com>
2022-04-07 19:11:57 +0600

    added initial data

--- Models/Song.cs
+++ Models/Song.cs
@@ -2,5 +2,17 @@
 {
     public class Song
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public string CoverPath { get; set; }
+        public string FilePath { get; set; }
+        public int GenreId { get; set; }
+        public int AuthorId { get; set; }
+        public int AlbumId { get; set; }
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+        public Album Album { get; set; }
+
     }
 }
--- Models/SongsList.cs
+++ Models/SongsList.cs
@@ -2,5 +2,9 @@
 {
     public class SongsList
     {
+        public int Id { get; set; }
+        public int SongId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
