abb15a9f1b918af9e46c66542a849c3e119916bd
Vlad Kislov <vadalize.vi@gmail.com>
2022-04-27 21:40:08 +0600

    music add in admin panel

--- Controllers/AdminAdd.cs
+++ Controllers/AdminAdd.cs
@@ -2,7 +2,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MusicPlayer.Data;
+using MusicPlayer.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicPlayer.Controllers
@@ -19,36 +23,67 @@ namespace MusicPlayer.Controllers
 
         public IActionResult AddPage()
         {
+            ViewBag.authors = db.Author.Select(x => x.Name).ToList();
+            ViewBag.genres = db.Genre.Select(x => x.Name).ToList();
+            ViewBag.albums = db.Album.Select(x => x.Name).ToList();
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(IFormFile upload)
+        public async Task<IActionResult> Add(List<IFormFile> upload, string name, string author, string genre, string album)
         {
-            string fileName = Path.GetFileName(upload.FileName);
-            string path="";
-            if (fileName.Contains(".jpg") || fileName.Contains(".png"))
+            string COVER = "";
+            string FILE = "";
+
+            var author_q = db.Author.FirstOrDefault(x => x.Name == author);
+            var genre_q = db.Genre.FirstOrDefault(x => x.Name == genre);
+            var album_q = db.Album.FirstOrDefault(x => x.Name == album);
+
+            if (author_q == null) author_q = new Author { Name = author };
+            if (genre_q == null) genre_q = new Genre { Name = genre };
+            if (album_q == null) album_q = new Album { Name = album, Date = DateTime.Now };
+
+            #region files upload
+            foreach (var file in upload)
             {
-                path = Path.Combine(this.ae.WebRootPath, "covers");
+                string fileName = Path.GetFileName(file.FileName);
+                string path = "";
+                if (fileName.Contains(".jpg") || fileName.Contains(".png"))
+                {
+                    COVER = fileName;
+                    path = Path.Combine(this.ae.WebRootPath, "covers");
+                }
+                else if (fileName.Contains(".mp3"))
+                {
+                    FILE = fileName;
+                    path = Path.Combine(this.ae.WebRootPath, "music");
+                }
+                else
+                {
+                    continue;
+                }
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
             }
-            else if (fileName.Contains(".mp3"))
+            #endregion
+
+            var res = new Song
             {
-                path = Path.Combine(this.ae.WebRootPath, "music");
-            }
-            else
-            {
-                return RedirectToAction("AddPage");
-            }
-            
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-            
-            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
-            {
-                upload.CopyTo(stream);
-            }
+                Name = name,
+                CoverPath = "covers/" + COVER,
+                FilePath = "music/" + FILE,
+                Genre = genre_q,
+                Author = author_q,
+                Album= album_q
+            };
+            db.Song.Add(res);
+            await db.SaveChangesAsync();
 
             return RedirectToAction("AddPage");
         }
